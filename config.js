8d3d7b03f30d701858c3209de5d261a6c54819f4
ktdprjct <97012512+ktdprjct@users.noreply.github.com>
2022-05-06 21:00:35 +0800

    update config.js

--- config.js
+++ config.js
@@ -15,11 +15,11 @@ global.clp = require('caliph-api')
 global.bs = require('@bochilteam/scraper')
 global.baileys = require('@adiwajshing/baileys')
 
-global.owner = JSON.parse(fs.readFileSync('./src/owner.json')) // Change owner number => src/premium.json
-global.mods = JSON.parse(fs.readFileSync('./src/moderator.json')) // Moderator?
+global.owner = ['62895342581896', '6285171612950', '6285877650700', '6281213812319'] // Put your number here
+global.mods = []
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Premium user has unlimited limit 
 
-global.repox = 'raselcomel/mymd'
+global.repox = 'ktdprjct/from-rasel'
 global.branx = 'master'
 
 global.APIs = { // API Prefix
@@ -45,8 +45,8 @@ global.APIKeys = { // APIKey Here
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = ''
-  var sticker_author = '               ig\n🔥 @rasel.ganz 🔥'
+  var sticker_name = 'wa.me/62895342581896'
+  var sticker_author = '© ktdprjct メ bot'
  } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -68,7 +68,7 @@ const cpack = fs.readFileSync("lib/colong.json")
 const ctickerpack = JSON.parse(cpack)
 if (ctickerpack.cpackname == '') {
   var colong_name = 'sikilinsut olang nylong stikell'
-  var colong_author = 'Ciss 📸\n\n@rasel.ganz'
+  var colong_author = '© ktdprjct メ bot'
  } else {
   var colong_name = ctickerpack.cpackname
   var colong_author = ctickerpack.cauthor
@@ -85,33 +85,33 @@ global.pclg = colong_name
 global.aclg = colong_author
 //======================================//
 
-global.wm = '@rasel.ganz'
+global.wm = '© ktdprjct メ bot'
 global.ucpn = ''
 
 //================= Url =================//
-global.linkgc = 'https://chat.whatsapp.com/CUCsW6BWfmJLJwJgPQIaKM'
-global.linkig = 'https://www.instagram.com/p/CH1A1c9J5pY/?utm_medium=copy_link'
+global.linkgc = 'https://chat.whatsapp.com/Jd3lmRL399k1KxARFmdLZO'
+global.linkig = ''
 global.linkyt = ''
-global.linkfb = 'https://fb.watch/aYv0jAffAO'
-global.web = 'https://raselcomel.github.io'
-global.web1 = 'https://anu.rasell.repl.co'
+global.linkfb = ''
+global.web = ''
+global.web1 = ''
 
 global.deslink = ''
-global.titlink = 'Folllow me on Instagram'
-global.bodlink = '@rasel.ganz'
+global.titlink = ''
+global.bodlink = ''
 
 //========= Url Template Button =========//
 global.dtu = 'Group Bot WhatsApp'
-global.urlnya = 'https://chat.whatsapp.com/CUCsW6BWfmJLJwJgPQIaKM'
+global.urlnya = 'https://chat.whatsapp.com/Jd3lmRL399k1KxARFmdLZO'
 
 //========= Call Template Button =========//
 global.dtc = 'Add Me'
-global.nmbrnya = '+62 822-5604-8971'
+global.nmbrnya = '+62 895-3425-81896'
 
 
 //============ Proses Bot ============//
-global.wait = 'Tunggu sebentar ya sayang'
-global.eror = 'Maaf terjadi kesalahan, silahkan coba lagi nanti'
+global.wait = '```Tunggu sebentar ya kak```'
+global.eror = 'error kk, coba lagi nanti'
 
 
 //========== Tampilan Bot ===========//
