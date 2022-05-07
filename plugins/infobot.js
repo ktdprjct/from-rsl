aa75a1b7df28d37362c7248f6a346a7e702d7da8
ktdprjct <97012512+ktdprjct@users.noreply.github.com>
2022-05-08 07:59:53 +0800

    update infobot.js

--- plugins/infobot.js
+++ plugins/infobot.js
@@ -19,7 +19,7 @@ ${gy} Library : *Baileys-MD*
 ${gy} Language : *Javascript*
 ${gy} Database : *MongoDB*
 ${gy} Version : *${versibot}*
-${gy} Developer : *@6285346545126*
+${gy} Developer : *@62895342581896*
 ${gy} Runtime : *${runtime}*
 ${gy} Prefix : *Multi Prefix 「 ${usedPrefix} 」*
 ${gy} Mode : *${global.opts['self'] ? 'Self' : 'Public'}*
@@ -29,8 +29,8 @@ ${sb}
 `.trim()
 var as = `Tanggal : ${week}, ${date}\nWaktu : ${a} (WIB)`
  //conn.sendTBL(m.chat, infonyacok, as, fla + `${command}`, `Source Code Bot ✨`, `https://github.com/raselcomel/lucubot-md`, null, null, `Menu`, `${usedPrefix}menu`, null, null, null, null, m, 
-     conn.sendBL(m.chat, infonyacok, as, fla + `${command}`, [[`SC Bot`, `${usedPrefix}sc`], [`Menu`, `${usedPrefix}menu`]], m,        
-           {mentions: ['6285346545126@s.whatsapp.net']})
+     conn.sendBL(m.chat, infonyacok, as, fla + `${command}`, [[`🏅Owner`, `${usedPrefix}owner`], [`Menu`, `${usedPrefix}menu`]], m,        
+           {mentions: ['62895342581896@s.whatsapp.net']})
 
 }
 handler.help = ['infobot']
